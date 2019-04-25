dbd1d9867ccc0d7facdc420abe4bc55d9a5a2d3d
mallikarjunputtana <mallikarjun.puttana@paconsulting.com>
2019-04-25 07:43:13 +0100

    new prokect

---
+++ src/main/java/subbuproject/subbuproject/App.java
@@ -0,0 +1,13 @@
+package subbuproject.subbuproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
